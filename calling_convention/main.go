bb26d36ecf4190cdb3d4c300e9de4c4bbc52d51a
adamtan <adamtan@tencent.com>
2020-02-29 13:14:38 +0800

    update concurrency with waitall, waitany, waitprio

--- calling_convention/main.go
+++ calling_convention/main.go
@@ -1,10 +1,82 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func calling(a int, b int) (c int, d int, e int) {
 	var k = 10
 	return a, b, k
 }
 
-func main() {
+func simpleCall() {
 	calling(12, 13)
+}
+
+func nestCall() {
+	var a = 10
+	nest := func() {
+		a = 20
+	}
+	nest()
+	fmt.Println(a)
+}
+
+func do(f func(int)) {
+	f(20)
+}
+
+func nestPass() {
+	var a = 10
+	do(func(b int) {
+		a = b
+	})
+	fmt.Println(a)
+}
+
+func nestGoroutine() {
+	var b = 20
+	var a = 10
+	var c = 30
+	fmt.Println(&b)
+	fmt.Println(&a)
+	fmt.Println(&c)
+
+	go func(b int) {
+		var c = 20
+		fmt.Println("xx", &c)
+		fmt.Println("go")
+		fmt.Println(&a)
+		fmt.Println(a)
+		a = b
+	}(20)
+	return
+}
+
+func nestGoroutine1() {
+	var b = 40
+	var a = 40
+	var c = 40
+	fmt.Println(&b)
+	fmt.Println(&a)
+	fmt.Println(&c)
+}
+
+func nestGoroutine2() {
+	var b = 40
+	var a = 40
+	var c = 40
+	fmt.Println(&b)
+	fmt.Println(&a)
+	fmt.Println(&c)
+}
+
+func main() {
+	var a = 10
+	fmt.Println("main", &a)
+	nestGoroutine()
+	nestGoroutine1()
+	nestGoroutine2()
+	time.Sleep(10 * time.Second)
 }
--- concurrency/main.go
+++ concurrency/main.go
@@ -5,186 +5,336 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"reflect"
 	"runtime"
 	"time"
 )
 
-func getCaller() string {
-	pc, _, _, ok := runtime.Caller(3)
-	if !ok {
-		return "[unknown] func"
+func setDefaultTimeout(ctx context.Context, t time.Duration) (context.Context, context.CancelFunc) {
+	var cancel context.CancelFunc = func() { log.Fatal("fuck") }
+	if _, ok := ctx.Deadline(); !ok {
+		ctx, cancel = context.WithTimeout(ctx, t)
 	}
-	details := runtime.FuncForPC(pc)
-	if details == nil {
-		return "[unknown] func"
-	}
-	return details.Name()
+	return ctx, cancel
 }
 
-func add(ctx context.Context, a, b int, ints ...int) (int, error) {
-	rand.Seed(time.Now().UnixNano()) //default rand source is goroutine safe
-	//always has deadline
+func getProcessTimeout(ctx context.Context) time.Duration {
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return time.Duration(0)
+	}
+	//empirical value 1/2
+	return deadline.Sub(time.Now()) / 2
+}
+
+type req interface{}
+type resp interface{}
+
+type respErr struct {
+	val resp
+	err error
+}
+
+func getHandlerName(h handler) string {
+	return runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+}
+
+type handler func(ctx context.Context, r req) (resp, error)
+type gather func(ctx context.Context, resps []resp) (resp, error)
+
+func getChan(chans []<-chan respErr, i int) <-chan respErr {
+	if i < len(chans) {
+		return chans[i]
+	}
+	return nil
+}
+
+//run handler simultaneously, wait for response and cancel signal
+func doHandleSub(ctx context.Context, c chan<- respErr, r req, h handler) {
+	//close c in only sender, indicates no more data can be read, needless here.
+	defer close(c)
+	waitResponse := func() <-chan respErr {
+		c := make(chan respErr)
+		go func() {
+			defer close(c)
+			var resp respErr
+			defer func() { c <- resp }()
+			resp.val, resp.err = h(ctx, r)
+		}()
+		return c
+	}
+	select {
+	case resp, _ := <-waitResponse():
+		log.Printf("[process] req: %+v, return: %+v, error: %+v\n", r, resp.val, resp.err)
+		c <- resp
+	case <-ctx.Done():
+		log.Printf("[context] req: %+v, error: %+v\n", r, ctx.Err())
+		c <- respErr{0, ctx.Err()}
+	}
+}
+
+//prepare channel response
+func doHandle(ctx context.Context, r req, h handler) <-chan respErr {
+	c := make(chan respErr, 1)
+	//take care when pass by reference!!
+	go doHandleSub(ctx, c, r, h)
+	return c
+}
+
+//set up channel and context communication with handler
+func prepare(ctx context.Context, reqs []req, handlers []handler) ([]<-chan respErr, context.CancelFunc) {
+	cancels := make([]context.CancelFunc, len(reqs))
+	cancel := func() {
+		for _, cancel := range cancels {
+			cancel()
+		}
+	}
+	chans := make([]<-chan respErr, len(reqs))
+	for i, req := range reqs {
+		reqCtx, reqCancel := context.WithTimeout(ctx, getProcessTimeout(ctx))
+		cancels[i] = reqCancel
+		chans[i] = doHandle(reqCtx, req, handlers[i])
+	}
+	return chans, cancel
+}
+
+type waitController interface {
+	//early return when recv response
+	earlyReturn(respErr) bool
+	//check if loop is over
+	breakLoop([]*respErr) bool
+}
+
+type waitPrioController int
+
+const waitPrioControllerConst = waitPrioController(0)
+
+//useless here, waitprio need global info, highest priority succeeded may early return
+func (waitPrioController) earlyReturn(r respErr) bool {
+	return false
+}
+
+//wait priority succeeded or all failed
+func (waitPrioController) breakLoop(resps []*respErr) bool {
+	allFailures := true
+	for _, resp := range resps {
+		if resp != nil {
+			if resp.err == nil {
+				return true
+			}
+		} else {
+			allFailures = false
+		}
+	}
+	return allFailures
+}
+
+//wait until any requests succeeded where all high priority requests are failed.
+func waitPrio(ctx context.Context, reqs []req, handlers []handler) ([]*respErr, error) {
+	return wait(ctx, reqs, handlers, waitPrioControllerConst)
+}
+
+type waitAnyController int
+
+const waitAnyControllerConst = waitAnyController(0)
+
+//any request succeeded
+func (waitAnyController) earlyReturn(r respErr) bool {
+	return r.err == nil
+}
+
+//all failed
+func (waitAnyController) breakLoop(resps []*respErr) bool {
+	for _, resp := range resps {
+		if resp == nil {
+			return false
+		}
+	}
+	return true
+}
+
+//wait until any request succeeded or all failed
+func waitAny(ctx context.Context, reqs []req, handlers []handler) ([]*respErr, error) {
+	return wait(ctx, reqs, handlers, waitAnyControllerConst)
+}
+
+type waitAllController int
+
+const waitAllControllerConst = waitAllController(0)
+
+func (waitAllController) earlyReturn(r respErr) bool {
+	return r.err != nil
+}
+func (waitAllController) breakLoop(resps []*respErr) bool {
+	for _, resp := range resps {
+		if resp == nil {
+			return false
+		}
+	}
+	return true
+}
+
+//wait until all request succeeded or any request failed
+func waitAll(ctx context.Context, reqs []req, handlers []handler) ([]*respErr, error) {
+	return wait(ctx, reqs, handlers, waitAllControllerConst)
+}
+
+func wait(ctx context.Context, reqs []req, handlers []handler, c waitController) ([]*respErr, error) {
+	if len(reqs) == 0 {
+		return nil, nil
+	}
+	if len(reqs) != len(handlers) {
+		return nil, fmt.Errorf("len(reqs) != len(handlers)")
+	}
+	chans, cancel := prepare(ctx, reqs, handlers)
+	defer cancel()
+
+	respErrs := make([]*respErr, len(reqs))
+	if len(chans) <= 1 {
+		//common case speed up
+		for {
+			if c.breakLoop(respErrs) {
+				break
+			}
+			//select: listen on both callees's channel returns and caller's context cancel.
+			select {
+			case RespErr, ok := <-getChan(chans, 0):
+				//unregister from select(https://golang.org/ref/spec#Select_statements)
+				chans[0] = nil
+				if !ok {
+					panic("never happened")
+				}
+				respErrs[0] = &RespErr
+				if c.earlyReturn(RespErr) {
+					fmt.Println("xx")
+					return respErrs, RespErr.err
+				}
+			case <-ctx.Done():
+				return respErrs, ctx.Err()
+			}
+		}
+	} else {
+		//dynamic way, readability & performance penalty(https://golang.org/pkg/reflect/#Select)
+		var cases = make([]reflect.SelectCase, len(reqs)+1)
+		for i, c := range chans {
+			cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(c)}
+		}
+		cases[len(cases)-1] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())}
+		for {
+			if c.breakLoop(respErrs) {
+				break
+			}
+			chosen, recv, recvOK := reflect.Select(cases)
+			if chosen == len(cases)-1 {
+				return respErrs, ctx.Err()
+			}
+			//runtime.reflect caseNil always continued, little performance lost
+			chans[chosen] = nil
+			cases[chosen] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(nil)}
+			if !recvOK {
+				fmt.Println(chosen)
+				panic("never happened")
+			}
+			RespErr, ok := recv.Interface().(respErr)
+			if !ok {
+				panic("never happened")
+			}
+			respErrs[chosen] = &RespErr
+			if c.earlyReturn(RespErr) {
+				return respErrs, RespErr.err
+			}
+		}
+	}
+	return respErrs, nil
+}
+
+/*
+	Top down goroutine call.
+	bottom up channel return.
+	Asynchronously top down context cancel which require callee’s explicitly listening.
+*/
+
+func concurrent(ctx context.Context, reqs []req, handlers []handler, g gather) (resp, error) {
+	//get Timeout with default timeout 4 seconds.
+	ctx, cancel := setDefaultTimeout(ctx, 4*time.Second)
+	defer cancel()
+	// if timeout < min processing time, return immediately
+
+	respErrs, err := waitAll(ctx, reqs, handlers)
+	if err != nil {
+		return 0, err
+	}
+
+	resps := make([]resp, 0, len(respErrs))
+	for _, respErr := range respErrs {
+		if respErr != nil && respErr.err == nil {
+			resps = append(resps, respErr.val)
+		}
+	}
+
+	return g(ctx, resps)
+}
+
+//do things in multi-stage style, check cancel signal at each stage.
+func sum(ctx context.Context, r []int) (int, error) {
 	deadline, _ := ctx.Deadline()
 	timeout := deadline.Sub(time.Now())
 	processTime := timeout * time.Duration(rand.Intn(1.2*100)) / 100
-	log.Printf("func: %s, deadline: %+v, process time: %s\n", getCaller(), timeout, processTime)
-	t := time.NewTimer(processTime)
-	defer t.Stop()
-	sum := func(a, b int, ints ...int) int {
-		t := a + b
-		for _, x := range ints {
-			t += x
+	log.Printf("[sum] req: %+v, timeout: %s, process time: %s\n", r, timeout, processTime)
+	s := 0
+	for i, v := range r {
+		t := time.NewTimer(processTime / time.Duration(len(r)))
+		defer t.Stop()
+		select {
+		case <-t.C:
+			if rand.Intn(10) == 100 {
+				return 0, fmt.Errorf("[sum] req: %+v process failed", r)
+			}
+			s += v
+		case <-ctx.Done():
+			log.Printf("[context] req: %+v, stage: %d, error: %+v\n", r, i+1, ctx.Err())
+			return 0, ctx.Err()
 		}
-		return t
 	}
-	select {
-	case <-t.C:
-		if rand.Intn(10) == 0 {
-			return 0, fmt.Errorf("func: %s process failed", getCaller())
-		}
-		return sum(a, b, ints...), nil
-	case <-ctx.Done():
-		log.Printf("func: %s canceled, error: %+v, resp: %+v\n", getCaller(), ctx.Err(), respErr{0, ctx.Err()})
-		return 0, ctx.Err()
-	}
-
-}
-
-func doAdd(ctx context.Context, c chan<- respErr, a, b int, ints ...int) {
-	//close c in sender, indicates no more data can be read, needless here.
-	defer close(c)
-	//always send
-	var resp respErr
-	defer func() { c <- resp }()
-	resp.val, resp.err = add(ctx, a, b, ints...)
-	return
-}
-
-func b(ctx context.Context, c chan<- respErr) {
-	doAdd(ctx, c, 1, 2)
-	return
-}
-
-func c(ctx context.Context, c chan<- respErr) {
-	doAdd(ctx, c, 3, 4)
-	return
-}
-
-func d(ctx context.Context, c chan<- respErr) {
-	doAdd(ctx, c, 5, 6)
-	return
-}
-
-type respErr struct {
-	val int
-	err error
+	return s, nil
 }
 
 /*
 	((1+2)+(3+4)+(5+6))
 	  ---   ---   ---
-	   b     c     d
+	   3  +  7  +  11
 	 -----------------
-	         a
-	Top down goroutine call.
-	bottom up channel return.
-	Asynchronously top down context cancel which require callee’s explicitly listening.
+			 21
+	run (1+2), (3+4), (5+6) simultaneously, wait all their returns (3, 7, 11) and sum them up together(3+7+11)
 */
-func a(ctx context.Context) (int, error) {
-	//add default Deadline, wait 4 seconds.
-	if _, ok := ctx.Deadline(); !ok {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeout(ctx, 4*time.Second)
-		defer cancel()
+func concurrentSum() {
+	sumHandler := func(ctx context.Context, r req) (resp, error) {
+		ints := r.([]int)
+		return sum(ctx, ints)
 	}
-	deadline, _ := ctx.Deadline()
-	timeout := deadline.Sub(time.Now())
-	// if timeout < min processing time, return immediately
-
-	bChan := make(chan respErr, 1)
-	var bVal int
-	//empirical value 1/2
-	bctx, bcancel := context.WithTimeout(ctx, timeout/2)
-	//alway cancel
-	defer bcancel()
-	//take care when pass by reference!!
-	go b(bctx, bChan)
-
-	cChan := make(chan respErr, 1)
-	var cVal int
-	cctx, ccancel := context.WithTimeout(ctx, timeout/2)
-	defer ccancel()
-	go c(cctx, cChan)
-
-	dChan := make(chan respErr, 1)
-	var dVal int
-	dctx, dcancel := context.WithTimeout(ctx, timeout/2)
-	defer dcancel()
-	go d(dctx, dChan)
-
-	for {
-		//select: listen on both callees's channel returns and caller's context cancel.
-		select {
-		case RespErr, ok := <-bChan:
-			log.Printf("func: main.b done, resp: %+v, ok: %+v\n", RespErr, ok)
-			/*
-				unregister bChan from select, reflect select can be more powerful with penalty cost(readability & performance)
-				#https://golang.org/ref/spec#Select_statements
-				#https://golang.org/pkg/reflect/#Select
-			*/
-			bChan = nil
-			if !ok {
-				panic("never happened")
-			}
-			if RespErr.err != nil {
-				//early return
-				return 0, RespErr.err
-			}
-			bVal = RespErr.val
-		case RespErr, ok := <-cChan:
-			log.Printf("func: main.c done, resp: %+v, ok: %+v\n", RespErr, ok)
-			cChan = nil
-			if !ok {
-				panic("never happened")
-			}
-			if RespErr.err != nil {
-				return 0, RespErr.err
-			}
-			cVal = RespErr.val
-		case RespErr, ok := <-dChan:
-			log.Printf("func: main.d done, resp: %+v, ok: %+v\n", RespErr, ok)
-			dChan = nil
-			if !ok {
-				panic("never happened")
-			}
-			if RespErr.err != nil {
-				return 0, RespErr.err
-			}
-			dVal = RespErr.val
-		case <-ctx.Done():
-			log.Printf("func: %s canceled\n", "main.a")
-			return 0, ctx.Err()
+	sumGather := func(ctx context.Context, resps []resp) (resp, error) {
+		ints := make([]int, len(resps))
+		for i, r := range resps {
+			ints[i] = r.(int)
 		}
-
-		if bChan == nil && cChan == nil && dChan == nil {
-			break
-		}
+		return sum(ctx, ints)
 	}
-
-	//process
-	var resultChan = make(chan respErr, 1)
-	doAdd(ctx, resultChan, bVal, cVal, dVal)
-	result := <-resultChan
-	return result.val, result.err
+	result, err := concurrent(context.Background(),
+		[]req{req([]int{1, 2}), req([]int{3, 4}), req([]int{5, 6})},
+		[]handler{sumHandler, sumHandler, sumHandler},
+		sumGather,
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	i := result.(int)
+	log.Printf("concurrentSum result: %d", i)
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano()) //default rand source is goroutine safe
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	val, err := a(context.Background())
-	if err != nil {
-		log.Println(err)
-	} else {
-		fmt.Println(val)
-	}
+	concurrentSum()
 	time.Sleep(5 * time.Second)
 }
